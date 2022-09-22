e62cd33a8a199c05574c3dd9794823412b7c9720
KateySt <kateystetsenko@gmail.com>
2022-09-22 19:25:04 +0300

    hw23

--- src/components/EntryField.js
+++ src/components/EntryField.js
@@ -3,7 +3,7 @@ import {connect, useDispatch} from "react-redux";
 import {setListF, setList} from "../features/inputThings/InputThingsSlice";
 import {bindActionCreators} from "redux";
 
-/*
+
 const EntryField = () => {
     const dispatch = useDispatch();
     const [textValue, setTextValue] = useState('');
@@ -32,58 +32,3 @@ const EntryField = () => {
 };
 
 export default EntryField;
- */
-class EntryField extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            text: '',
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange() {
-        const handleChangeCounter = this.props.actions;
-        handleChangeCounter(this.state.text);
-    }
-
-    render() {
-        return (
-            <div>
-                <input
-                    type="text"
-                    value={this.props.text}
-                    onChange={(e) => {
-                        this.setState({text: e.target.value})
-                    }}
-                />
-                <button
-                    type="submit"
-                    onClick={this.handleChange}
-                >
-                    Input
-                </button>
-            </div>
-        );
-    }
-}
-
-const mapStateToProps = function (state) {
-    return {
-        counter: state.inputThings.value,
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        actions: bindActionCreators(setListF, dispatch)
-    }
-}
-
-const mergeProps = (stateProps, dispatchProps, ownProps) => {
-    return Object.assign({}, ownProps, stateProps, dispatchProps);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(EntryField);
