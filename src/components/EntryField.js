4ba6b06cc01628dd49c7e9c87110c29756e732d0
KateySt <kateystetsenko@gmail.com>
2022-09-22 19:28:30 +0300

    hw24

--- src/components/EntryField.js
+++ src/components/EntryField.js
@@ -4,31 +4,57 @@ import {setListF, setList} from "../features/inputThings/InputThingsSlice";
 import {bindActionCreators} from "redux";
 
 
-const EntryField = () => {
-    const dispatch = useDispatch();
-    const [textValue, setTextValue] = useState('');
+class EntryField extends React.Component {
+    constructor(props) {
+        super(props);
 
-    function handleChange() {
-        dispatch(setListF(textValue));
+        this.state = {
+            text: '',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    return (
-        <div>
-            <input
-                type="text"
-                value={textValue}
-                onChange={(text) => {
-                    setTextValue(text.target.value)
-                }}
-            />
-            <button
-                type="submit"
-                onClick={handleChange}
-            >
-                Input
-            </button>
-        </div>
-    );
-};
+    handleChange() {
+        const handleChangeCounter = this.props.actions;
+        handleChangeCounter(this.state.text);
+    }
 
-export default EntryField;
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.props.text}
+                    onChange={(e) => {
+                        this.setState({text: e.target.value})
+                    }}
+                />
+                <button
+                    type="submit"
+                    onClick={this.handleChange}
+                >
+                    Input
+                </button>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = function (state) {
+    return {
+        counter: state.inputThings.value,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(setListF, dispatch)
+    }
+}
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+    return Object.assign({}, ownProps, stateProps, dispatchProps);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(EntryField);
